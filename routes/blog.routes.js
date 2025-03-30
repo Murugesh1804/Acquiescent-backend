a18341fdbfcc9070faf8e1fd5a04b43d3660c89f
murugesh <dhanamurugesh1804@gmail.com>
2025-03-30 17:59:02 +0530

    Initial commit

---
+++ routes/blog.routes.js
@@ -0,0 +1,25 @@
+import express from "express"
+import {
+  getAllBlogs,
+  getBlogBySlug,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+  getBlogCategoriesAndTags,
+} from "../controllers/blog.controller.js"
+import { protect, optionalAuth } from "../middleware/auth.middleware.js"
+
+const router = express.Router()
+
+// Public/Optional Auth routes
+router.get("/all", getAllBlogs)
+router.get("/slug/:slug", getBlogBySlug)
+router.get("/categories-tags", getBlogCategoriesAndTags)
+
+// Protected routes
+router.post("/create", protect, createBlog)
+router.put("/update/:id", updateBlog)
+router.post("/delete/:id", deleteBlog)
+
+export default router
+
